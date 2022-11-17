e8b4a6e0cd363daf0af67afbc827102c47fb08e4
CezikLikeWhat <cezarymackowski99@gmail.com>
2022-11-17 19:56:33 +0100

    Updated AddUser command

--- src/User/Application/UseCase/AddUser.php
+++ src/User/Application/UseCase/AddUser.php
@@ -7,7 +7,6 @@ namespace App\User\Application\UseCase;
 use App\Core\Domain\Email;
 use App\Core\Domain\Exception\EmailException;
 use App\Core\Infrastructure\Symfony\Uuid4;
-use App\User\Application\UseCase\AddUser\Command;
 use App\User\Domain\Exception\UserException;
 use App\User\Domain\Repository\UserRepository;
 use App\User\Domain\User;
@@ -25,16 +24,15 @@ class AddUser
      * @throws UserException
      * @throws EmailException
      */
-    public function __invoke(Command $command): void
+    public function __invoke(AddUser\Command $command): void
     {
         $user = new User(
-            Uuid4::generateNew(),
-            $command->googleId,
-            $command->firstName,
-            $command->lastName,
-            new Email($command->email),
-            $command->devices,
-            $command->roles
+            id: $command->userId ?? Uuid4::generateNew(),
+            googleId: $command->googleId,
+            firstName: $command->firstName,
+            lastName: $command->lastName,
+            email: new Email($command->email),
+            roles: $command->roles
         );
 
         $this->userRepository->add($user);
