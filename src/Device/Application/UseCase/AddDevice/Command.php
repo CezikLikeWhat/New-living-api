6481dfcaf7df220215e662e558c0c4dca6ea1169
CezikLikeWhat <cezarymackowski99@gmail.com>
2022-11-22 19:44:32 +0100

    Updated AddDevice command

--- src/Device/Application/UseCase/AddDevice/Command.php
+++ src/Device/Application/UseCase/AddDevice/Command.php
@@ -13,7 +13,6 @@ class Command
         public readonly string $name,
         public readonly string $deviceType,
         public readonly string $macAddress,
-        public readonly ?\DateTimeImmutable $createdAt = null,
         public readonly ?Uuid $id = null,
     ) {
     }
