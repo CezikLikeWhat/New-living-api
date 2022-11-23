5520167dff91daa3d842f0ca4636ad494c20a339
CezikLikeWhat <cezarymackowski99@gmail.com>
2022-11-23 14:10:39 +0100

    Added message class and handler

---
+++ src/Core/Application/Message/DeviceStatus.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Application\Message;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class DeviceStatus
+{
+    public function __construct(
+
+    ) {
+    }
+
+    public function __invoke(DeviceStatus\Command $command): void
+    {
+    }
+}
