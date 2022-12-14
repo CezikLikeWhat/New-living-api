de11bb5637e8dcaff743f557739d242ae62b8564
CezikLikeWhat <cezarymackowski99@gmail.com>
2022-12-14 10:43:06 +0100

    Added query class

---
+++ src/Core/Application/Query/UserQuery/DeviceWithFeatures.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Application\Query\UserQuery;
+
+use App\Device\Domain\DeviceType;
+use App\Device\Domain\MACAddress;
+
+class DeviceWithFeatures
+{
+    /**
+     * @param array{
+     *     feature_name: string,
+     *     display_type: string,
+     *     payload: array<mixed>
+     * }[] $features
+     */
+    public function __construct(
+        public readonly string $deviceName,
+        public readonly DeviceType $deviceType,
+        public readonly MACAddress $macAddress,
+        public readonly \DateTimeImmutable $createdAt,
+        public readonly array $features,
+    ) {
+    }
+}
